8e9a7be20f3f82cb27e9f63e242c7fe7fc11c1ed
justin landaverde <justlandaverde28@gmail.com>
2014-10-01 15:03:04 -0700

    uploading the fake blog post to an array of strings

--- app/src/main/java/andriod/landaverdej/blogreader/BlogActivity.java
+++ app/src/main/java/andriod/landaverdej/blogreader/BlogActivity.java
@@ -2,6 +2,7 @@ package andriod.landaverdej.blogreader;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 
@@ -15,11 +16,20 @@ public class BlogActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blog);
+        String[] arrayStrings = new String[]{
+                "Blog Post 1",
+                "Blog Post 2",
+                "Blog Post 3",
+                "Blog Post 4"
+
+        };
 
         progressBar = (ProgressBar)findViewById(R.id.ProgressBar);
         listView = (ListView)findViewById(R.id.ListView);
 
         listView.setEmptyView(progressBar);
+        ArrayAdapter<String> adapter= new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrayStrings);
+        listView.setAdapter(adapter);
     }
 
 
