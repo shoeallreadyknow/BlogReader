1684410426f1b5ca889f79f287dea6b3a2755ba8
justin landaverde <justlandaverde28@gmail.com>
2014-10-01 14:44:39 -0700

    got the progress bar working on my tablet

--- app/src/main/java/andriod/landaverdej/blogreader/BlogActivity.java
+++ app/src/main/java/andriod/landaverdej/blogreader/BlogActivity.java
@@ -2,35 +2,26 @@ package andriod.landaverdej.blogreader;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.ListView;
+import android.widget.ProgressBar;
 
 
 public class BlogActivity extends Activity {
+    protected ProgressBar progressBar;
+    protected ListView listView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blog);
+
+        progressBar = (ProgressBar)findViewById(R.id.ProgressBar);
+        listView = (ListView)findViewById(R.id.ListView);
+
+        listView.setEmptyView(progressBar);
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.blog, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
