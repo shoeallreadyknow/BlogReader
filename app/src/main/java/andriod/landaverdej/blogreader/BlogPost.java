8fdd3d7ee2be77e83d5124b17a37cc9f62ec5b74
justin landaverde <justlandaverde28@gmail.com>
2014-10-02 10:48:26 -0700

    finished have of the blog post file

---
+++ app/src/main/java/andriod/landaverdej/blogreader/BlogPost.java
@@ -0,0 +1,13 @@
+package andriod.landaverdej.blogreader;
+
+public class BlogPost {
+    public final String tittle;
+    public final String url;
+
+    public BlogPost(String tittle, String url){
+
+    }
+
+}
+
+
