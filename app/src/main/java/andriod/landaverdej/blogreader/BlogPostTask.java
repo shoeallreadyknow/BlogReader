17e7fc3bf521199945432f0bfba96c96d81dfd41
justin landaverde <justlandaverde28@gmail.com>
2014-10-23 14:36:37 -0700

    storing parsed JSON Object

--- app/src/main/java/andriod/landaverdej/blogreader/BlogPostTask.java
+++ app/src/main/java/andriod/landaverdej/blogreader/BlogPostTask.java
@@ -15,6 +15,9 @@ import java.net.URL;
 public class BlogPostTask  extends AsyncTask<Activity,Void,JSONObject> {
     @Override
     protected JSONObject doInBackground(Activity... activities) {
+      JSONObject jsonObject = null;
+
+
         try {
             URL blogFeedUrl = new URL("http://blog.teamtreehouse.com/api/get_recent_summary/?count=10");
             HttpURLConnection connection = (HttpURLConnection)blogFeedUrl.openConnection();
@@ -22,7 +25,7 @@ public class BlogPostTask  extends AsyncTask<Activity,Void,JSONObject> {
             int responseCode = connection.getResponseCode();
             if(responseCode == HttpURLConnection.HTTP_OK){
                 Log.i("BlogPostTask", "Successful Connection " + responseCode);
-                BlogPostParser.get().parse(connection.getInputStream());
+          jsonObject= BlogPostParser.get().parse(connection.getInputStream());
             }
 
         }
@@ -32,6 +35,12 @@ public class BlogPostTask  extends AsyncTask<Activity,Void,JSONObject> {
         catch(IOException error){
             Log.e("BlogPostTask", "IO Exception: " + error);
         }
-        return null;
+        return jsonObject;
+    }
+
+    @Override
+    protected void onPostExecute(JSONObject jsonObject) {
+        super.onPostExecute(jsonObject);
+        
     }
 }
