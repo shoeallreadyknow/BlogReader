f71c986b783ac6ebd2c381318c59c6fd9beaf984
justin landaverde <justlandaverde28@gmail.com>
2014-11-07 14:24:59 -0800

    creating a blogpost adapter to display list items

---
+++ app/src/main/java/andriod/landaverdej/blogreader/BlogPostAdapter.java
@@ -0,0 +1,15 @@
+package andriod.landaverdej.blogreader;
+
+
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BlogPostAdapter extends ArrayAdapter<BlogPost> {
+    public BlogPostAdapter(Context context, ArrayList<BlogPost> posts) {
+        super(context, 0, posts);
+
+    }
+}
