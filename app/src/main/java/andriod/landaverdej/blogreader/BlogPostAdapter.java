418121edf812a874f2ab2908a28d7a5b954811d0
justin landaverde <justlandaverde28@gmail.com>
2014-11-07 14:40:32 -0800

    inflating a layout

--- app/src/main/java/andriod/landaverdej/blogreader/BlogPostAdapter.java
+++ app/src/main/java/andriod/landaverdej/blogreader/BlogPostAdapter.java
@@ -2,10 +2,11 @@ package andriod.landaverdej.blogreader;
 
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-
+s
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,7 +19,10 @@ public class BlogPostAdapter extends ArrayAdapter<BlogPost> {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         BlogPost post= getItem(position);
-        
+
+        if(convertView == null){
+            convertView = LayoutInflater.from(getContext()).inflate(android.R.layout.simple_list_item_1,parent,false)
+        }
 
     }
 
